bb1677ea4e986ae89c50aa917371dc88f2245236
Daniel Rudnitski <danielrudn@gmail.com>
2022-09-17 18:29:03 -0400

    initial commit

---
+++ lib/ex_curl.ex
@@ -0,0 +1,18 @@
+defmodule ExCurl do
+  @moduledoc """
+  Documentation for `ExCurl`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExCurl.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_curl_test.exs
@@ -0,0 +1,8 @@
+defmodule ExCurlTest do
+  use ExUnit.Case
+  doctest ExCurl
+
+  test "greets the world" do
+    assert ExCurl.hello() == :world
+  end
+end
