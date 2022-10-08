e06dd83d5fcb3e3882b4cb34bddc181a2cb5af86
Rolf Håvard Blindheim <rolf.havard.blindheim@intility.no>
2022-10-09 00:13:03 +0200

    more debug

--- test/support/files/setup_kerberos.sh
+++ test/support/files/setup_kerberos.sh
@@ -72,5 +72,3 @@ service krb5-kdc restart
 
 echo "*** Getting ticket for Kerberos user"
 echo -n "$KERBEROS_PASSWORD" | kinit "$KERBEROS_USERNAME@${KERBEROS_REALM^^}"
-klist -k /etc/krb5.keytab
-ls /tmp/
