36363a5ee13865ca06ca39d624e2a809f3388e0a
Daniel Rudnitski <danielrudn@gmail.com>
2024-08-19 21:24:15 -0400

    chore(devenv): bump to zig 0.13

--- devenv.nix
+++ devenv.nix
@@ -2,7 +2,7 @@
 let unstable-pkgs = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
 in {
   packages = [
-    unstable-pkgs.zig_0_12
+    unstable-pkgs.zig_0_13
     unstable-pkgs.elixir_1_17
     pkgs.curl
   ];
