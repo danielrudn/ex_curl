1074edbba60bbb86e59552b0847c6be065251d3a
Daniel Rudnitski <danielrudn@gmail.com>
2026-07-26 02:41:23 -0400

    refactor: use dirty_io and set curl options via comptime table (#19)

--- test/support/test_client.ex
+++ test/support/test_client.ex
@@ -1,8 +1,4 @@
 defmodule ExCurl.TestClient do
-  @moduledoc """
-  There is a race condition when listening for connections
-  and making requests to localhost simulataneously. Setting `dirty_cpu` to `true`
-  will remove the race condition.
-  """
-  use ExCurl.Client, defaults: [dirty_cpu: true]
+  @moduledoc false
+  use ExCurl.Client
 end
